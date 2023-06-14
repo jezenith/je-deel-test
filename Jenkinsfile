cfd2f6a07e6914b2fbff8e6f3058f42388d64d64
Jonathan Emmanuel <88687066+jezenith@users.noreply.github.com>
2023-06-14 11:55:58 +0100

    helm

---
+++ Jenkinsfile
@@ -0,0 +1,67 @@
+pipeline {
+    agent any
+
+    environment {
+        registry = "jezenith/je-deel-test"
+        registryCredential = 'dockerhub'
+        scannerHome = tool 'mysonarscanner4'
+        PATH = "${env.PATH}:/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/mysonarscanner4/bin"
+    }
+
+    stages {
+
+        stage('Install Dependencies') {
+            steps {
+                sh 'pip install -r requirements.txt'
+                sh 'pip install pylint'
+            }
+        }
+
+        stage('Unit Test') {
+            steps {
+                sh 'python3 -m unittest'
+            }
+        }
+
+        stage('Pylint') {
+            steps {
+                sh 'pylint **/*.py | tee pylint-report.txt'
+            }
+        }
+
+        stage('Building image') {
+            steps {
+                script {
+                    dockerImage = docker.build registry + ":$BUILD_NUMBER"
+                }
+            }
+        }
+
+        stage('Deploy Docker Image') {
+            steps {
+                script {
+                    docker.withRegistry( '', registryCredential ) {
+                        dockerImage.push("$BUILD_NUMBER")
+                        dockerImage.push('latest')
+                    }
+                }
+            }
+        }
+
+        stage('Remove Unused docker image') {
+            steps {
+                sh "docker rmi $registry:$BUILD_NUMBER"
+            }
+        }
+
+        stage('Deploy to Kubernetes') {
+            agent { label 'KOPS' }
+            steps {
+                sh "kubectl create namespace prod || true"
+                sh "helm upgrade --install --force je-deel-test je-deel-test-chart/ --set appimage=${registry}:${BUILD_NUMBER} --namespace prod"
+            }
+        }
+
+    }
+
+}
---
+++ app.py
@@ -0,0 +1,66 @@
+import os
+import logging
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+# Set up logging
+logging.basicConfig(filename='app.log', level=logging.ERROR)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('RDS_USERNAME')}:{os.getenv('RDS_PASSWORD')}@je-deel.cpl9h4c99kwt.us-east-2.rds.amazonaws.com:5432/je-deel"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.wsgi_app = ProxyFix(app.wsgi_app)  # To get the correct IP behind a proxy (like Nginx)
+
+db = SQLAlchemy(app)
+
+class IP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reversed_ip = db.Column(db.String(15), nullable=False)
+
+@app.route('/')
+def display_ip():
+    try:
+        ip = request.remote_addr
+        reversed_ip = '.'.join(ip.split('.')[::-1])
+        existing_ip = IP.query.filter_by(reversed_ip=reversed_ip).first()
+        if not existing_ip:
+            new_ip = IP(reversed_ip=reversed_ip)
+            db.session.add(new_ip)
+            db.session.commit()
+        return render_template('index.html', ip=ip, reversed_ip=reversed_ip)
+    except Exception as e:
+        app.logger.error(f"Error occurred: {e}")
+        return render_template('error.html'), 500
+
+@app.route('/all')
+def display_all():
+    try:
+        ips = IP.query.all()
+        reversed_ips = [ip.reversed_ip for ip in ips]
+        return render_template('all.html', reversed_ips=reversed_ips)
+    except Exception as e:
+        app.logger.error(f"Error occurred: {e}")
+        return render_template('error.html'), 500
+
+def create_db():
+    with app.app_context():
+        db.create_all()
+
+if __name__ == '__main__':
+    create_db()
+    app.run(host='0.0.0.0', port=5001)
+
+    @app.route('/health')
+def health_check():
+    try:
+        # Run a simple SELECT query
+        result = db.engine.execute('SELECT 1')
+        # If the query was successful, return a positive response
+        if result.fetchone()[0] == 1:
+            return 'Database connection successful', 200
+    except Exception as e:
+        # If there was an error, log it and return a negative response
+        app.logger.error(f"Database connection failed: {e}")
+        return 'Database connection failed', 500
+

a181c5bd552530ce84fc508099c205890cd8997f
Jonathan Emmanuel <88687066+jezenith@users.noreply.github.com>
2023-06-14 12:43:55 +0100

    helm

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-<<<<<<< HEAD
 import logging
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -13,13 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('RDS_USERNAME'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 app.wsgi_app = ProxyFix(app.wsgi_app)  # To get the correct IP behind a proxy (like Nginx)
 
-=======
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ips.db'
->>>>>>> origin/main
 db = SQLAlchemy(app)
 
 class IP(db.Model):
@@ -29,7 +21,6 @@ class IP(db.Model):
 @app.route('/')
 def display_ip():
     try:
-<<<<<<< HEAD
         ip = request.remote_addr
         reversed_ip = '.'.join(ip.split('.')[::-1])
         existing_ip = IP.query.filter_by(reversed_ip=reversed_ip).first()
@@ -40,44 +31,16 @@ def display_ip():
         return render_template('index.html', ip=ip, reversed_ip=reversed_ip)
     except Exception as e:
         app.logger.error(f"Error occurred: {e}")
-=======
-        # Get client IP
-        # If X-Forwarded-For header is present, use it and get the first IP if multiple IPs are present
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(",")[0]
-        # Reverse the IP
-        reversed_ip = '.'.join(ip.split('.')[::-1])
-        # Store the reversed IP in the database
-        new_ip = IP(reversed_ip=reversed_ip)
-        db.session.add(new_ip)
-        db.session.commit()
-        # Render the HTML template with the IP and reversed IP
-        return render_template('index.html', ip=ip, reversed_ip=reversed_ip)
-    except Exception as e:
-        app.logger.error(f"Error occurred: {e}")
-        # Render error page if exception occurs
->>>>>>> origin/main
         return render_template('error.html'), 500
 
 @app.route('/all')
 def display_all():
     try:
-<<<<<<< HEAD
         ips = IP.query.all()
         reversed_ips = [ip.reversed_ip for ip in ips]
         return render_template('all.html', reversed_ips=reversed_ips)
     except Exception as e:
         app.logger.error(f"Error occurred: {e}")
-=======
-        # Query all reversed IPs from the database
-        ips = IP.query.all()
-        # Convert the results to a list of reversed IPs
-        reversed_ips = [ip.reversed_ip for ip in ips]
-        # Render a template with the list of reversed IPs
-        return render_template('all.html', reversed_ips=reversed_ips)
-    except Exception as e:
-        app.logger.error(f"Error occurred: {e}")
-        # Render error page if exception occurs
->>>>>>> origin/main
         return render_template('error.html'), 500
 
 def create_db():
@@ -85,7 +48,6 @@ def create_db():
         db.create_all()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     create_db()
     app.run(host='0.0.0.0', port=5001)
 
@@ -102,10 +64,3 @@ def health_check():
         app.logger.error(f"Database connection failed: {e}")
         return 'Database connection failed', 500
 
-=======
-    if os.getenv('WERKZEUG_RUN_MAIN') != 'true':
-        create_db()
-    # Run the application without reloader
-    from werkzeug.serving import run_simple
-    run_simple('localhost', 5001, app, use_reloader=False)
->>>>>>> origin/main

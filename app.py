6c478b540e2b7bb8b0ec8c4c8e89f6276d5a2c3a
Jonathan Emmanuel <88687066+jezenith@users.noreply.github.com>
2023-06-15 16:07:44 +0100

    health-page

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 logging.basicConfig(filename='app.log', level=logging.ERROR)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('RDS_USERNAME')}:{os.getenv('RDS_PASSWORD')}@je-deel.cpl9h4c99kwt.us-east-2.rds.amazonaws.com:5432/je-deel"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 app.wsgi_app = ProxyFix(app.wsgi_app)  # To get the correct IP behind a proxy (like Nginx)
 
@@ -42,6 +42,20 @@ def display_all():
     except Exception as e:
         app.logger.error(f"Error occurred: {e}")
         return render_template('error.html'), 500
+    
+
+@app.route('/health')
+def health_check():
+    try:
+        # Run a simple SELECT query
+        result = db.session.query(IP).first()
+        # If the query was successful, return a positive response
+        return render_template('health.html', message='Database connection successful', css_class='success'), 200
+    except Exception as e:
+        # If there was an error, log it and return a negative response
+        app.logger.error(f"Database connection failed: {e}")
+        return render_template('health.html', message='Database connection failed', css_class='failure'), 500
+
 
 def create_db():
     with app.app_context():
@@ -50,17 +64,3 @@ def create_db():
 if __name__ == '__main__':
     create_db()
     app.run(host='0.0.0.0', port=5001)
-
-    @app.route('/health')
-def health_check():
-    try:
-        # Run a simple SELECT query
-        result = db.engine.execute('SELECT 1')
-        # If the query was successful, return a positive response
-        if result.fetchone()[0] == 1:
-            return 'Database connection successful', 200
-    except Exception as e:
-        # If there was an error, log it and return a negative response
-        app.logger.error(f"Database connection failed: {e}")
-        return 'Database connection failed', 500
-
